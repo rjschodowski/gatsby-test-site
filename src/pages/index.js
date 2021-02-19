79d199fd8c5fe6874381791305b7b1cb81442d65
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-02-19 17:58:35 -0500

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Dude, this is great practice. You are killing this!!</h1>
+    <p>Keep up the great work, man.</p>
+    <h2>You can learn on this, then will come your first app. Be excited!!</h2>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
