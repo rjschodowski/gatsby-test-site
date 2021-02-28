cdb74bf57b993c559bb1b9968fe0f4be2734d324
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-02-28 12:02:23 -0500

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Menu from "../components/menu"
+
+const AboutPage = () => (
+        <Layout>
+            <Menu />
+            <h1>About Us</h1>
+            <p>SOS Lansing was created to help newly-arriving individuals and families, citizens and non-citizens, 
+             become acquainted with the city of Lansing's emergency services and resources.</p>
+        </Layout>
+    )
+
+export default AboutPage;
