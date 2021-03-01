7af23387cd653ea0c5b1a0c5a3fcf409c285dc32
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-01 18:46:17 -0500

    Add files via upload

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,9 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.rjschodowski.com">R.J.</a>
         </footer>
       </div>
     </>
