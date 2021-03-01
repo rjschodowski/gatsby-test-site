020c9c61d38316989266104b832e1777688e3a0e
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-01 18:44:12 -0500

    Add files via upload

--- src/pages/medical.js
+++ src/pages/medical.js
@@ -9,6 +9,7 @@ const Medical = () => (
             <p>Sparrow Hospital</p>
             <p><span>Address: </span>1215 E Michigan Ave, Lansing, MI 48912</p>
             <p><span>Phone: </span>(517) 364-1000</p>
+            <p><span>Website: </span><a href="https://www.sparrow.org/our-hospitals-and-services/sparrow-hospitals/sparrow-hospital">Sparrow Hospital </a></p>
         </Layout>
     )
 
