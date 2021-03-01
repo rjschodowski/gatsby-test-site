de1c71e6891a33f880ec33503c1cf471ea2f4a95
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-01 18:43:58 -0500

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import CallLogo from "../components/call"
+import Menu from "../components/menu"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Dude, this great practice, mon ami. You can do it!</h1>
-    <CallLogo src={"images/gatsby-icon.png"} alt={"phone call icon"} />
-    <p>Keep up the great work, man.</p>
-    <h2>You can learn on this, then will come your first app. Be excited!!</h2>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h2>I need...</h2>
+    <Menu />
   </Layout>
 )
 
