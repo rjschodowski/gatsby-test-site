472b71f5b4a8a528ad6133fd74936e238a3bb197
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-02 17:38:50 -0500

    Add files via upload

---
+++ src/pages/financial.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const Financial = () => (
+        <Layout>
+            <h1>Financial</h1>
+            <p>MDHHS Cash Assistance</p>
+            <p><span>Address: </span>5303 S Cedar St, Lansing, MI 48911</p>
+            <p><span>Phone: </span>(517) 887-9400</p>
+            <p><span>Website: </span><a href="https://www.michigan.gov/mdhhs/0,5885,7-339-71547_5526---,00.html">MDHHS Cash Assistance</a></p>
+        </Layout>
+    )
+
+export default Financial;
