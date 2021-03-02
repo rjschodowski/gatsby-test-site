662db150f68db6fb9cd409ebc6d0b9c8f36164f5
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-02 17:38:11 -0500

    Add files via upload

---
+++ src/pages/legal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const Legal = () => (
+        <Layout>
+            <h1>Legal</h1>
+            <p>St. Vincent Catholic Charities Immigration Law Clinic</p>
+            <p><span>Address: </span>2800 W. Willow St., Lansing, MI 48917</p>
+            <p><span>Phone: </span>(517) 323-4734<span> EXT. 1800</span></p>
+            <p><span>Website: </span><a href="https://stvcc.org/services/immigration/">St. Vincent Catholic Charities Immigration Law Clinic</a></p>
+        </Layout>
+    )
+
+export default Legal;
