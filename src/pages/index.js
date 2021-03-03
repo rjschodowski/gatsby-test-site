87783a34fa9c4fb9fee5608c81abad7221357c7a
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2021-03-03 18:16:34 -0500

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Menu from "../components/menu"
@@ -7,7 +6,6 @@ import Menu from "../components/menu"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h2>I need...</h2>
     <Menu />
   </Layout>
 )
